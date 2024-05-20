3474e40f33e6830a506f7a41cbe6f71cefe5e339
Junnoh Lee <pluruel@gmail.com>
2024-05-20 09:49:26 +0900

    build for linux

--- build.sh
+++ build.sh
@@ -12,10 +12,10 @@ else
     DIR_OS="Linux"
 fi
 
-if [[ "$*" == *"--ios"* ]]; then
-    DIR_OS="iOS"
-elif [[ "$*" == *"--android"* ]]; then
-    DIR_OS="Android"
-fi
+# if [[ "$*" == *"--ios"* ]]; then
+#     DIR_OS="iOS"
+# elif [[ "$*" == *"--android"* ]]; then
+#     DIR_OS="Android"
+# fi
 
 python3 $DIR/tools/ci_build/build.py --build_dir $DIR/build/$DIR_OS "$@"
